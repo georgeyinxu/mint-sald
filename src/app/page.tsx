1682dc9d42268ceb7975d987aca0604ee929b8c9
George Yin Xu <89299295+georgeyinxu@users.noreply.github.com>
2023-10-04 09:32:44 +0800

    added upgrades to instantly change network to mumbai

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,28 @@
 "use client";
 
-import { ConnectWallet, useAddress } from "@thirdweb-dev/react";
+import {
+  ConnectWallet,
+  useAddress,
+  useNetworkMismatch,
+  useNetwork,
+  ChainId,
+} from "@thirdweb-dev/react";
+import { useEffect } from "react";
 import MintCard from "./components/MintCard";
 
 export default function Home() {
   const address = useAddress();
+  const isMismatched = useNetworkMismatch();
+  const [, switchNetwork] = useNetwork(); // Switch to desired chain
+
+  useEffect(() => {
+    // Check if the user is connected to the wrong network
+    if (isMismatched) {
+      // Prompt their wallet to switch networks
+      switchNetwork(ChainId.Mumbai); // the chain you want here
+    }
+  }, [address]); // This above block gets run every time "address" changes (e.g. when the user connects)
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       {address ? <MintCard /> : <ConnectWallet />}
--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -6,12 +6,12 @@ import {
   ThirdwebProvider,
   walletConnect,
 } from "@thirdweb-dev/react";
-import { Mumbai, Polygon } from "@thirdweb-dev/chains";
+import { Mumbai } from "@thirdweb-dev/chains";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ThirdwebProvider
-      supportedChains={[Mumbai, Polygon]}
+      supportedChains={[Mumbai]}
       clientId="c39b8ec1e949b0b1fa56a439cb8f311c"
       supportedWallets={[metamaskWallet(), walletConnect()]}
     >
